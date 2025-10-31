fe0748192dde06c49485f9a2836d342428fd423b
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-10-31 18:00:22 +0700

    [FEAT] Create Tabel with Prisma Database

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require ('dotenv').config ();
 const express = require ('express')
-
-
+var cors = require('cors')
 const app = express();
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
